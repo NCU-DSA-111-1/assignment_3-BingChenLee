33df171b5f416f43ed1354f7286332a0f6ac5bbd
bingchen <tony98168@gmail.com>
2023-01-05 15:32:27 +0800

    second commit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/bingchen/110503518_assignment_4/Data_Structures_Assignment4/arcd
+# Build directory: /home/bingchen/110503518_assignment_4/Data_Structures_Assignment4/arcd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("arcd")
+subdirs("examples")
+subdirs("tests")
---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/bingchen/110503518_assignment_4/Data_Structures_Assignment4/arcd/tests
+# Build directory: /home/bingchen/110503518_assignment_4/Data_Structures_Assignment4/arcd/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(codec_tests "/home/bingchen/110503518_assignment_4/Data_Structures_Assignment4/arcd/tests/codec_tests")
+set_tests_properties(codec_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/bingchen/110503518_assignment_4/Data_Structures_Assignment4/arcd/tests/CMakeLists.txt;10;add_test;/home/bingchen/110503518_assignment_4/Data_Structures_Assignment4/arcd/tests/CMakeLists.txt;0;")
